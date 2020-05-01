986856c82dbe0a2083cacdf353c0cfec5ce5f20f
Garrett <goodbyetonto@gmail.com>
2020-05-01 10:28:22 -0700

    I am using a attr (opacity) change for hours from 12am to 1 hour prior to the current time, and an attr (read only)change for the same hour blocks, to keep users from updating content in the past.

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 $(document).ready(function () {
 
-    // Create variable to hold the hour value (0 - 23) of the current time, as an integer
-    var currentHour = parseInt(moment().hours());
-
     // Create variable to save the value of the class .btn
     const saveBtn = $(".btn");
 
     // Create variable to save the value of the class .form-control
     var idArray = [];
 
+
     $('.form-control').each(function () {
         idArray.push(parseInt(this.id));
     });
+
+    function pastPres(currentHour) {
+
+        // Create variable to hold the hour value (0 - 23) of the current time, as an integer
+        var currentHour = parseInt(moment().hours());
+
+        // Create new array from 0 to currentHour (exlusive)
+        var past = idArray.slice(0, currentHour);
+
+        // for each 'hour' in array, change the class for each .0 through .23 classes peretaining to the hour divs
+        for (hour of past) {
+            $(`.${hour}`).css("opacity", ".2");
+            $(`#${hour}`).attr("readonly", true); 
+        }
+    };
 
     // Define timer function
     function timer() {
@@ -24,12 +37,13 @@ $(document).ready(function () {
 
     // Call Timer function
     timer();
+    pastPres();
 
     // Get each ToDo item from local storage and place back in respective form fields with class .form-control
     for (i of idArray) {
         var currentID = $(`#${i}`);
         var refill = localStorage.getItem(`ToDo ${i}`);
-        currentID.val(refill); 
+        currentID.val(refill);
     }
 
     // Create variable to hold the hour value (0 - 23) of the current time, as an integer
@@ -41,7 +55,6 @@ $(document).ready(function () {
         // Prevent default behavior after click so as not to clear the form text
         event.preventDefault();
 
-
         // Loop through each div element with the class txtField
         for (i of idArray) {
 
@@ -50,15 +63,8 @@ $(document).ready(function () {
 
             // Store in local memory the text that is input into the txtField div
             localStorage.setItem(`ToDo ${i}`, currentID.val());
-
         }
-
-
     });
-
-    // if($(this).is(":empty")){
-    //     $(this).css("background", "yellow");
-    // }
 })
 
 
